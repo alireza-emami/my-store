a862e5434b4e55252c1e387af4f66a59e2e372a0
Alireza Emami <aliemturbo@gmail.com>
2023-12-27 12:52:00 +0330

    1

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,12 @@ import LoginPage from "@/components/LoginPage";
 
 const Login = () => {
   return (
-    <>
-      <LoginPage />
-    </>
+    <div className="mt-7">
+      <div className="flex flex-col  items-center">
+        <p className="">Please Create an acount and go for the best shope. </p>
+      </div>
+        <LoginPage />
+    </div>
   );
 };
 
--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -4,50 +4,156 @@ import { useState } from "react";
 
 const LoginPage = () => {
   const [username, setUserName] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+  const [number, setNumber] = useState<number>();
+  const [zipcode, setZipcode] = useState("");
+  const [lat, setLat] = useState<number>();
+  const [long, setLong] = useState<number>();
+  const [phone, setPhone] = useState<number>();
 
   const HandleSubmit = () => {
     fetch("https://fakestoreapi.com/users", {
       method: "POST",
       body: JSON.stringify({
-        email: "moz@gmail.com",
+        email: email,
         username: username,
         password: password,
         name: {
-          firstname: "alireza",
-          lastname: "emami",
+          firstname: firstname,
+          lastname: lastname,
         },
         address: {
-          city: "shiraz",
-          street: "7835 new road",
-          number: 33,
-          zipcode: "111111111",
+          city: city,
+          street: street,
+          number: number,
+          zipcode: zipcode,
           geolocation: {
-            lat: "-37.3159",
-            long: "81.1496",
+            lat: lat,
+            long: long,
           },
         },
-        phone: "1111111",
+        phone: phone,
       }),
     })
       .then((res) => res.json())
       .then((json) => console.log(json));
   };
   return (
-    <div>
-      <Input
-        type="text"
-        placeholder="email"
-        value={username}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <Input
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button onClick={HandleSubmit}>submit</Button>
+    <div >
+      <div className="flex flex-col p-6">
+        <h1 className="text-center">Peronal Informations</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+          <Input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="text"
+            placeholder="firstname"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="text"
+            placeholder="lastname"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            className="p-2"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col p-6">
+        <h1 className="text-center">Address</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 ">
+          <Input
+            type="text"
+            placeholder="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="text"
+            placeholder="street"
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="number"
+            placeholder="number"
+            value={`${number}`}
+            onChange={(e:any) => setNumber(e.target.value)}
+            className="p-2"
+          />
+          <Input
+            type="text"
+            placeholder="zipcode"
+            value={zipcode}
+            onChange={(e) => setZipcode(e.target.value)}
+            className="p-2"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col p-6">
+        <h1 className="text-center">Geolocation</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 ">
+          <Input
+            type="number"
+            placeholder="lat"
+            value={`${lat}`}
+            onChange={(e:any) => setLat(e.target.value)}
+            className="p-2 "
+          />
+          <Input
+            type="number"
+            placeholder="long"
+            value={`${long}`}
+            onChange={(e:any) => setLong(e.target.value)}
+            className="p-2 "
+          />
+        </div>
+      </div>
+      <div className="flex flex-col p-6">
+        <h1 className="text-center">Phone</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 ">
+          {" "}
+          <Input
+            type="number"
+            placeholder="phone"
+            value={`${phone}`}
+            onChange={(e:any) => setPhone(e.target.value)}
+            className="p-2"
+          />
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Button onClick={HandleSubmit}>submit</Button>
+      </div>
     </div>
   );
 };
